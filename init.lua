7693029f6b10a0a0541ad0a58c8413250527481a
anhoder <anhoderai@xiaoman.cn>
2023-10-31 15:05:09 +0800

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.o.guifont = "ComicMono NF:h16"
+
+vim.g.neovide_transparency = 0.9
+vim.g.transparency = 0.9
+vim.g.neovide_theme = "auto"
+vim.g.neovide_confirm_quit = true
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
