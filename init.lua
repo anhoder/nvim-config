bffc02451f4de28c9f00c2a78737f9b7b4245a93
anhoder <anhoderai@xiaoman.cn>
2023-10-31 18:03:31 +0800

    update

--- init.lua
+++ init.lua
@@ -2,10 +2,10 @@
 require("config.lazy")
 
 vim.o.guifont = "ComicMono NF:h16"
+vim.opt.linespace = 2
 
-vim.g.neovide_transparency = 0.9
-vim.g.transparency = 0.9
-vim.g.neovide_theme = "auto"
+vim.g.neovide_transparency = 0.95
+vim.g.transparency = 0.95
 vim.g.neovide_confirm_quit = true
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_animate_in_insert_mode = true
