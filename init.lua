0eaae1e4d28b0676e40aa40c8467023926a9160c
anhoder <anhoder@88.com>
2023-11-02 10:48:52 +0800

    feat(keymap): add keymap to resize window
    <C-D-Left/Right/Up/Down>
    
    Signed-off-by: anhoder <anhoder@88.com>

--- init.lua
+++ init.lua
@@ -15,3 +15,4 @@ vim.g.neovide_cursor_animate_command_line = true
 -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
 -- vim.g.neovide_cursor_vfx_particle_density = 15.0
 vim.g.neovide_fullscreen = true
+vim.g.neovide_input_macos_alt_is_meta = true
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-D-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-D-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-D-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-D-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
