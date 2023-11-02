8b4efe02867921f76de76c89310708945161a060
anhoder <anhoder@88.com>
2023-11-02 22:50:50 +0800

    feat(keymap): add Copy/Paste/Cut keymap for system clipboard
    add Copy/Paste/Cut keymap for system clipboard
    
    Signed-off-by: anhoder <anhoder@88.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,13 +5,18 @@
 local map = vim.keymap.set
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-D-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-D-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-D-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-D-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map({ "n", "i", "v" }, "<C-D-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map({ "n", "i", "v" }, "<C-D-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map({ "n", "i", "v" }, "<C-D-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map({ "n", "i", "v" }, "<C-D-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move to window using the <ctrl> hjkl keys
-map("n", "<C-A-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<C-A-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
-map("n", "<C-A-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
-map("n", "<C-A-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+map({ "n", "i", "v" }, "<C-A-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+map({ "n", "i", "v" }, "<C-A-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map({ "n", "i", "v" }, "<C-A-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map({ "n", "i", "v" }, "<C-A-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+map({ "n", "v" }, "<D-c>", '"+y', { desc = "Copy", noremap = true })
+map({ "n", "v" }, "<D-v>", '"+p', { desc = "Paste", noremap = true })
+map({ "n", "v" }, "<D-x>", '"+x', { desc = "Cut", noremap = true })
+map("i", "<D-c>", "<C-r>+", { desc = "Paste", noremap = true })
