c5dbe13747dd27903af2ddefda837ad6abefed3d
anhoder <anhoder@88.com>
2023-11-07 10:09:31 +0800

    update

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,5 +1,13 @@
 return {
   "folke/flash.nvim",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      search = {
+        enabled = false,
+      },
+    },
+  },
   keys = {
     -- disable the default flash keymap
     { "s", mode = { "n", "x", "o" }, false },
