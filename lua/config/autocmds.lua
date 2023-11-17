3da4fc953fe0e2b7ea1ec62f4b8f90e7a0ee9b35
anhoder <anhoder@88.com>
2023-11-17 20:00:24 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -35,3 +35,12 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
   desc = "Disable focus autoresize for FileType",
 })
+
+local group_cdpwd = vim.api.nvim_create_augroup("GroupCdpwd", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
+  end,
+})
