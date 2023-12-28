40d0099224e889dbdeccd28db4b3e25b6cc4ba50
anhoder <anhoder@88.com>
2023-12-28 21:10:41 +0800

    update

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -11,5 +11,13 @@ return {
   keys = {
     -- disable the default flash keymap
     { "s", mode = { "n", "x", "o" }, false },
+    {
+      "D",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
   },
 }
